5c62a314c5a50ebe78f435a0f9ba9de12d9fd3ff
Ryan Soklaski <ry26099@mit.edu>
2020-08-08 12:55:24 -0400

    add some functions

--- ghosty/__init__.py
+++ ghosty/__init__.py
@@ -1,4 +1,14 @@
 from .objects import Person
+import numpy as np
+
 
 def identity(x: Person) -> Person:
     return x
+
+
+def add1(x: float, y: float) -> float:
+    return float(np.nan_to_num(x + y))
+
+
+def add2(x: float, y: float) -> float:
+    return float(np.nan_to_num(x + y))
--- ghosty/objects.py
+++ ghosty/objects.py
@@ -1,6 +1,17 @@
+__all__ = ["as_int", "as_Person", "Person"]
+
+
 class Person:
     def __init__(self, x: int):
-        self.x = x
+        self.x: int = x
 
     def __eq__(self, other: "Person"):
         return self.x == other.x
+
+
+def as_int(x: Person) -> int:
+    return x.x
+
+
+def as_Person(x: int) -> Person:
+    return Person(x)
